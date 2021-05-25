03146820d03ce6f33cf7c0b75b842e364311651b
ha4hi0 <37325679+ha4hi0@users.noreply.github.com>
2021-05-26 06:25:42 +0900

    テストで表示する行列を間違えていたのを修正

--- test.c
+++ test.c
@@ -13,25 +13,50 @@ int main(void){
 	show_image(&(train_data->images[100]));
 
 	fprintf(stderr, "\ntest matrix calc and rand init\n");
+
 	Matrix C;
 	C.row = 3;
 	C.column = 5;
 	C.val = (float*)malloc(sizeof(float)*C.row*C.column);
 	rand_init(&C);
+
 	Matrix B;
 	B.row = 5;
 	B.column = 4;
 	B.val = (float*)malloc(sizeof(float)*B.row*B.column);
 	rand_init(&B);
+
 	Matrix A;
 	A.row = 3;
 	A.column = 4;
 	A.val = (float*)malloc(sizeof(float)*A.row*A.column);
-	mul(&A, &B, &C);
-	fprintf(stderr, "A:\n");
-	show(&A);
+
+	fprintf(stderr, "C:\n");
+	show(&C);
+
 	fprintf(stderr, "\nB:\n");
 	show(&B);
-	fprintf(stderr, "\nAB=C:\n");
-	show(&C);
+
+	fprintf(stderr, "\nA=CB:\n");
+	show(mul(&A, &B, &C));
+
+	Matrix D;
+	D.row = 3;
+	D.column = 5;
+	D.val = (float*)malloc(sizeof(float)*D.row*D.column);
+
+	rand_init(&D);
+	fprintf(stderr, "\nD:\n");
+	show(&D);
+
+	Matrix E;
+	E.row = 3;
+	E.column = 5;
+	E.val = (float*)malloc(sizeof(float)*E.row*E.column);
+
+	fprintf(stderr, "\nE=C+D:\n");
+	show(plus(&E, &D, &C));
+
+	fprintf(stderr, "\nE=2.0*D:\n");
+	show(scalar(&E, &D, 2.0));
 }
